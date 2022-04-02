d48e25a90ce219a61dee537836e5397c09e054b4
jiang.wang <jiang.wang@opay-inc.com>
2022-04-02 16:13:00 +0800

    feat: del
    Signed-off-by: jiang.wang <jiang.wang@opay-inc.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,72 +1,48 @@
-/**
- * @module .eslintrc
- * @description: eslint配置
- *
- * 所需插件
- * prettier // 规则见 https://prettier.io/docs/en/options.html
- * eslint // 规则见 https://cn.eslint.org/docs/rules/
- * eslint-plugin-vue 规则见 https://github.com/vuejs/eslint-plugin-vue
- * eslint-plugin-prettier // 将prettier作为ESLint规范来使用
- * eslint-config-prettier
- * @typescript-eslint/eslint-plugin
- * @typescript-eslint/parser // ESLint的解析器，用于解析typescript，从而检查和规范Typescript代码
- *
- * "off" 或 0 - 关闭规则
- * "warn" 或 1 - 开启规则，使用警告级别的错误：warn (不会导致程序退出)
- * "error" 或 2 - 开启规则，使用错误级别的错误：error (当被触发的时候，程序会退出)
- */
-
 module.exports = {
   root: true,
   env: {
     browser: true,
     node: true,
+    'vue/setup-compiler-macros': true
   },
-  globals: {
-    Message: true,
-    env: true,
-    useRoute: true,
-    useRouter: true,
-    useStore: true,
-  },
-  parser: "vue-eslint-parser",
   parserOptions: {
-    parser: "@typescript-eslint/parser",
+    parser: '@typescript-eslint/parser',
     ecmaVersion: 2020,
-    sourceType: "module",
+    sourceType: 'module'
   },
   extends: [
-    "vue-global-api",
-    "eslint:recommended",
-    "plugin:vue/vue3-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "plugin:prettier/recommended",
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier'
   ],
-  plugins: ["vue", "prettier", "@typescript-eslint"],
+  plugins: ['vue', 'prettier', '@typescript-eslint'],
   rules: {
-    "prettier/prettier": ["off"],
-    "vue/multi-word-component-names": "off",
-    "no-console": process.env.NODE_ENV === "production" ? 1 : 0,
-    "no-debugger": process.env.NODE_ENV === "production" ? 1 : 0,
-    "max-len": ["error", { code: 100, tabWidth: 2 }],
-    eqeqeq: 2,
-    "vue/eqeqeq": 2,
-    "no-undef": 2,
-    "vue/require-v-for-key": 1,
-    "no-unused-vars": 0,
-    "vars-on-top": 0,
-    "prefer-destructuring": 0,
-    "consistent-return": 0,
-    camelcase: 0,
-    "array-callback-return": 1,
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 100,
+        tabWidth: 2,
+        tabs: false,
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'none'
+      },
+      {
+        usePrettierrc: false
+      }
+    ],
+    'vue/multi-word-component-names': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 1 : 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 1 : 0,
 
-    "vue/no-multiple-template-root": 0,
-    "@typescript-eslint/explicit-module-boundary-types": 0,
-    "@typescript-eslint/no-var-requires": 0,
-    "@typescript-eslint/no-unused-vars": 0,
-    "@typescript-eslint/ban-ts-comment": 0,
-    "@typescript-eslint/no-explicit-any": 0,
-    "@typescript-eslint/no-empty-function": 0,
-  },
-};
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/no-unused-vars': 0,
+    '@typescript-eslint/ban-ts-comment': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-empty-function': 0
+  }
+}
--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -2,15 +2,23 @@
   <div class="inner-box">
     <h3 class="text-4xl font-bold text-center text-blue-500">index</h3>
     <h1 class="mt-6 text-2xl font-bold text-gray-900 leading-tight w-48 h-1/2">
-      MIRRORS
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis non necessitatibus optio
+      excepturi, quae consequatur quam in rem sint fuga saepe quasi impedit quia consectetur
+      placeat, aliquam perferendis inventore dolore!
     </h1>
     <p class="mt-8 text-2xl text-blue-200">Hello</p>
     <p class="font-bold">12</p>
   </div>
 </template>
+
 <script setup lang="ts">
-console.log("123");
+console.log('123')
+const a = {
+  abc: 1,
+  b: 3
+}
 </script>
+
 <style>
 .inner-box {
   border: 1px solid blue;
