690ddbe827f659c2262758501c5391626e1cf07c
jiang.wang <jiang.wang@opay-inc.com>
2022-04-02 14:44:25 +0800

    feat: e
    Signed-off-by: jiang.wang <jiang.wang@opay-inc.com>

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,17 +3,17 @@
  * @description: eslint配置
  */
 module.exports = {
-  // printWidth: 100,
-  // tabWidth: 2,
-  // tabs: false,
-  // semi: false,
-  // singleQuote: true,
-  // quoteProps: 'as-needed',
-  // bracketSpacing: true,
-  // arrowParens: 'always',
-  // proseWrap: 'always', // 当超出print width（上面有这个参数）时就折行
-  // htmlWhitespaceSensitivity: 'ignore', // 指定 HTML 文件的全局空白区域敏感度, "ignore" - 空格被认为是不敏感的
-  // vueIndentScriptAndStyle: false, // 在VUE文件中不要缩进脚本和样式标记
-  // stylelintIntegration: true,
-  // endOfLine: 'auto',
-};
+  printWidth: 100,
+  tabWidth: 2,
+  tabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  proseWrap: 'always', // 当超出print width（上面有这个参数）时就折行
+  htmlWhitespaceSensitivity: 'ignore', // 指定 HTML 文件的全局空白区域敏感度, "ignore" - 空格被认为是不敏感的
+  vueIndentScriptAndStyle: false, // 在VUE文件中不要缩进脚本和样式标记
+  stylelintIntegration: true,
+  endOfLine: 'auto',
+}
